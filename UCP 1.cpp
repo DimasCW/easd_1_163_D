4bed5a255d0b9d7fb323baea6a1e158e54c4059f
DimasCW <cdimas292@gmail.com>
2023-04-07 09:41:27 +0700

    menyelesaikan soal 1 sampai 5

--- UCP 1.cpp
+++ UCP 1.cpp
@@ -2,5 +2,8 @@
 1. Karena algoritma dibutuhkan untuk menyelesaikan masalah secara sistematis
 2. Statis & Dinamis
 3. membuat codingan yang tidak diperlukan
-
+4. Menurut saya Insertion sort karena membandingkan dan memindahkan elemen ke posisi yang tepat didalam susunan data yang sudah diurutkan
+5. Quadratic : Bubble Sort, Selection Sort, dan Insertion Sort
+	Loglinear : Merge Sort dan Quick Sort
+6. Selection sort: 
 **/
