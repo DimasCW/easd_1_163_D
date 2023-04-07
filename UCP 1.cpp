9fd1a481ea94d99dce9b30e8b9f2649c77b172ce
DimasCW <cdimas292@gmail.com>
2023-04-07 10:10:30 +0700

    membuat void input

--- UCP 1.cpp
+++ UCP 1.cpp
@@ -7,3 +7,50 @@
 	Loglinear : Merge Sort dan Quick Sort
 6. Selection sort: 
 **/
+#include<iostream>
+using namespace std;
+
+
+
+const int MAX_DATA = 2 * 15 + 10 + 20; // batas maksimum data
+
+void input(int arr[], int n) {
+    cout << "Masukkan " << n << " angka:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    for (int FA = 0; FA <= n - 2; FA++) { // ubah j menjadi FA sesuai persyaratan
+        int min_index = FA;
+        for (int i = FA + 1; i < n; i++) {
+            if (arr[i] < arr[min_index]) {
+                min_index = i;
+            }
+        }
+        swap(arr[FA], arr[min_index]);
+    }
+}
+
+void display(int arr[], int n) {
+    cout << "Data yang telah diurutkan: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[MAX_DATA];
+    int n = 0;
+
+    cout << "Masukkan jumlah data (maksimum " << MAX_DATA << "): ";
+    cin >> n;
+
+    input(arr, n);
+    selectionSort(arr, n);
+    display(arr, n);
+
+    return 0;
+}
