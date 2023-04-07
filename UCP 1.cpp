0afc2eabea16b0758da3a2873853f568c7d0435b
DimasCW <cdimas292@gmail.com>
2023-04-07 10:48:36 +0700

    menambahkan yang kurang

--- UCP 1.cpp
+++ UCP 1.cpp
@@ -1,7 +1,7 @@
 /**
 1. Karena algoritma dibutuhkan untuk menyelesaikan masalah secara sistematis
 2. Statis & Dinamis
-3. membuat codingan yang tidak diperlukan
+3. membuat codingan yang tidak diperlukan, pemilihan algoritma, ukuran memori, dan ukuran input 
 4. Menurut saya Insertion sort karena membandingkan dan memindahkan elemen ke posisi yang tepat didalam susunan data yang sudah diurutkan
 5. Quadratic : Bubble Sort, Selection Sort, dan Insertion Sort
 	Loglinear : Merge Sort dan Quick Sort
